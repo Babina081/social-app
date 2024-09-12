eb768f15df5c49533d3d7dbbb7d5084ae0f03c02
Babina081 <babitamoo333@gmail.com>
2024-09-12 22:02:24 +0545

    add fetch post from database

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,22 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
+import Post from "@/components/posts/Post";
+import prisma from "@/lib/prisma";
+import { postDataInclude } from "@/lib/types";
 
-export default function page() {
+export default async function Home() {
+  const post = await prisma.post.findMany({
+    include: postDataInclude,
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
   return (
-    <main className="h-[200vh] w-full bg-red-50">
-      <div className="w-full">
+    <main className="min-w-0 w-full">
+      <div className="w-full min-w-0 space-y-5">
         <PostEditor></PostEditor>
+        {post.map((post) => (
+          <Post key={post.id} post={post}></Post>
+        ))}
       </div>
     </main>
   );
