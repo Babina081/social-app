8d61efb5ad415887293dc9bb37da4db029a32de5
Babina081 <babitamoo333@gmail.com>
2024-09-12 18:38:09 +0545

    add Post Editor component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import Navbar from "./Navbar";
+import PostEditor from "@/components/posts/editor/PostEditor";
 
 export default function page() {
   return (
     <main className="h-[200vh] w-full bg-red-50">
-      <div className="w-full">front page</div>
+      <div className="w-full">
+        <PostEditor></PostEditor>
+      </div>
     </main>
   );
 }
