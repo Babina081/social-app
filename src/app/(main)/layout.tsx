ce08896db64f33e98f4cf5587f7958f7764ca018
Babina081 <babitamoo333@gmail.com>
2024-09-12 16:47:09 +0545

    add responsive side menu bar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { redirect } from "next/navigation";
 import SessionProvider from "./SessionProvider";
 import Navbar from "./Navbar";
+import MenuBar from "./MenuBar";
 
 export default async function MainLayout({
   children,
@@ -15,7 +16,12 @@ export default async function MainLayout({
     <SessionProvider value={session}>
       <div className="flex min-h-screen flex-col">
         <Navbar></Navbar>
-        <div className="mx-auto max-w-7xl p-5">{children}</div>
+
+        <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+          <MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80"></MenuBar>
+          {children}
+        </div>
+        <MenuBar className="sticky bottom-0 flex w-full justify-center border-t bg-card p-3 sm:hidden"></MenuBar>
       </div>
     </SessionProvider>
   );
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import Navbar from "./Navbar";
 
 export default function page() {
-  return <main>page</main>;
+  return (
+    <main className="h-[200vh] w-full bg-red-50">
+      <div className="w-full">front page</div>
+    </main>
+  );
 }
