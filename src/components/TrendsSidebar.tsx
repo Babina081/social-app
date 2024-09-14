efd49517673f7096286d29e53d9209eb2da8126b
Babina081 <babitamoo333@gmail.com>
2024-09-14 18:39:23 +0545

    update trend sidebar

--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -1,13 +1,13 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { userDataSelect } from "@/lib/types";
-import { Loader2 } from "lucide-react";
-import Link from "next/link";
-import React, { Suspense } from "react";
-import UserAvatar from "./UserAvatar";
-import { Button } from "./ui/button";
-import { unstable_cache } from "next/cache";
+import { getUserDataSelect } from "@/lib/types";
 import { formatNumber } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+import { unstable_cache } from "next/cache";
+import Link from "next/link";
+import { Suspense } from "react";
+import FollowButton from "./FollowButton";
+import UserAvatar from "./UserAvatar";
 
 export default function TrendsSidebar() {
   return (
@@ -24,19 +24,24 @@ async function WhoToFollow() {
   const { user } = await validateRequest();
 
   if (!user) return null;
-  await new Promise((r) => setTimeout(r, 1000));
+
   const usersToFollow = await prisma.user.findMany({
     where: {
       NOT: {
         id: user.id,
       },
+      followers: {
+        none: {
+          followerId: user.id,
+        },
+      },
     },
-    select: userDataSelect,
+    select: getUserDataSelect(user.id),
     take: 5,
   });
 
   return (
-    <div className="space-y-5 rounded-xl bg-card p-5 shadow-sm">
+    <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
       <div className="text-xl font-bold">Who to follow</div>
       {usersToFollow.map((user) => (
         <div key={user.id} className="flex items-center justify-between gap-3">
@@ -44,10 +49,7 @@ async function WhoToFollow() {
             href={`/users/${user.username}`}
             className="flex items-center gap-3"
           >
-            <UserAvatar
-              avatarUrl={user.avatarUrl}
-              className="flex-none"
-            ></UserAvatar>
+            <UserAvatar avatarUrl={user.avatarUrl} className="flex-none" />
             <div>
               <p className="line-clamp-1 break-all font-semibold hover:underline">
                 {user.displayName}
@@ -57,7 +59,15 @@ async function WhoToFollow() {
               </p>
             </div>
           </Link>
-          <Button>Follow</Button>
+          <FollowButton
+            userId={user.id}
+            initialState={{
+              followers: user._count.followers,
+              isFollowedByUser: user.followers.some(
+                ({ followerId }) => followerId === user.id,
+              ),
+            }}
+          />
         </div>
       ))}
     </div>
@@ -67,26 +77,31 @@ async function WhoToFollow() {
 const getTrendingTopics = unstable_cache(
   async () => {
     const result = await prisma.$queryRaw<{ hashtag: string; count: bigint }[]>`
-  SELECT LOWER(unnest(regexp_matches(content,'#[[:alnum:]_]+','g'))) AS hashtag,COUNT(*) AS count FROM posts
-  GROUP BY (hashtag)
-  ORDER BY count DESC,hashtag ASC
-  LIMIT 5
-  `;
+            SELECT LOWER(unnest(regexp_matches(content, '#[[:alnum:]_]+', 'g'))) AS hashtag, COUNT(*) AS count
+            FROM posts
+            GROUP BY (hashtag)
+            ORDER BY count DESC, hashtag ASC
+            LIMIT 5
+        `;
+
     return result.map((row) => ({
       hashtag: row.hashtag,
       count: Number(row.count),
     }));
   },
   ["trending_topics"],
-  { revalidate: 3 * 60 * 60 },
+  {
+    revalidate: 3 * 60 * 60,
+  },
 );
 
 async function TrendingTopics() {
-  const trendingtopics = await getTrendingTopics();
+  const trendingTopics = await getTrendingTopics();
+
   return (
     <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
-      <div className="text-xl font-bold">Trending Topics</div>
-      {trendingtopics.map(({ hashtag, count }) => {
+      <div className="text-xl font-bold">Trending topics</div>
+      {trendingTopics.map(({ hashtag, count }) => {
         const title = hashtag.split("#")[1];
         return (
           <Link key={title} href={`/hashtag/${title}`} className="block">
