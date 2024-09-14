111185a150e072378ccc1a5c0bd6094fd9cd84b7
Babina081 <babitamoo333@gmail.com>
2024-09-14 18:39:40 +0545

    update getPostDataInclude

--- src/components/posts/actions.ts
+++ src/components/posts/actions.ts
@@ -2,16 +2,15 @@
 
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
+import { getPostDataInclude } from "@/lib/types";
 
 export async function deletePost(id: string) {
   const { user } = await validateRequest();
+
   if (!user) throw new Error("Unauthorized");
 
   const post = await prisma.post.findUnique({
-    where: {
-      id,
-    },
+    where: { id },
   });
 
   if (!post) throw new Error("Post not found");
@@ -20,8 +19,8 @@ export async function deletePost(id: string) {
 
   const deletedPost = await prisma.post.delete({
     where: { id },
-    include: postDataInclude,
+    include: getPostDataInclude(user.id),
   });
-  
+
   return deletedPost;
 }
